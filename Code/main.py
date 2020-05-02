49967f112b5bf2463de6d3b7ccfbc5b32f48a8ea
braun <simon.braun@rwth-aachen.de>
2020-05-02 16:22:02 +0200

    Changes on main.py

---
+++ Code/main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+input_path = '../input/Exporting parcel structure per sending country (parameters).xlsx'
+a = pd.read_excel(input_path)
+print(a.head())
+
+print('Test für Git')
